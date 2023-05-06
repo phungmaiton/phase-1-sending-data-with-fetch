9b79919298547e9f542006a6a3a7e4491395ad51
Mai <garcia.ton94@gmail.com>
2023-05-06 18:03:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+const submitData = (name, email) => {
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(
+            {
+                "name": name,
+                "email": email,
+            }
+        )  
+  })
+    .then(resp => resp.json())
+    .then(object => {
+        let id = object.id;
+        let body = document.querySelector('body');
+        body.append(id);
+    })
+    .catch(error => {
+        let message = error;
+        let body = document.querySelector('body');
+        body.append(message);
+    })
+}
